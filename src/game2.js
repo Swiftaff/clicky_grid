a74a1055a1a4ecc0dba36fd1a5ea025cfceaece1
swiftaff <swiftaff@gmail.com>
2025-04-18 10:55:51 +0800

    WIP nested templates re-rendering correctly

--- src/game2.js
+++ src/game2.js
@@ -1,15 +1,19 @@
 app.init(
   {
-    testy2: {
+    size_buttons1: {
       for_array: [2, 3, 4],
       //for_item: "thingy",
       //for_index: "num",
       class_toggles: [["highlight", (item, idx) => app.store.size == item]],
     },
-    testy3: {
+    size_buttons2: {
       for_array: [5, 6, 7],
       class_toggles: [["highlight", (item, idx) => app.store.size == item]],
     },
+    grid_new: {
+      for_array: [0, 1, 2, 3],
+    },
+    outer_grid: {},
   },
   {
     size: 2,
--- src/silly.js
+++ src/silly.js
@@ -1,9 +1,18 @@
 const app = {
   init,
   reset_dom,
+  templates: [],
 };
 
 function init(obj, store = {}) {
+  console.log();
+  console.log("====");
+  console.log("init");
+  //create hoisted div to temporarily contain nested templates later
+  app.hoisted = document.createElement("div");
+  app.hoisted.style = "display: none; visibility:hidden;";
+  document.body.appendChild(app.hoisted);
+
   app.components = obj;
   app.store = {
     ...store,
@@ -16,69 +25,130 @@ function init(obj, store = {}) {
 }
 
 function reset_dom() {
+  console.log();
+  console.log("====");
   console.log("reset_dom");
 
   //find and delete previously added dom elements
-  const silly_templates = document.querySelectorAll("[data-is_silly]");
-  console.log(silly_templates);
+  console.log(app.templates);
+  const silly_els = document.querySelectorAll("[data-is_silly]");
+  let silly_templates = Object.values(app.templates).flatMap((tpl) =>
+    Array.from(tpl.content.querySelectorAll("[data-is_silly]")),
+  );
+  silly_templates = [...silly_templates, ...silly_els];
+  console.log("silly templates", silly_templates);
   silly_templates.forEach((t) => t.remove());
 
   //find templates in the dom
-  const templates = document.querySelectorAll("template[component]");
-  app.templates = Array.from(templates).reduce((map, tpl) => {
-    const name = tpl.getAttribute("component");
-    if (name) map[name] = tpl;
-    return map;
-  }, {});
+  const { templates, nested_templates } = collect_templates();
+  console.log("templates", templates, nested_templates);
+  app.templates = Array.from([...nested_templates, ...templates]).reduce(
+    (map, tpl) => {
+      const name = tpl.getAttribute("component");
+      if (name) map[name] = tpl;
+      return map;
+    },
+    {},
+  );
   console.log(app.templates);
 
-  Object.keys(app.components).map((component_name) => {
-    console.log("name", component_name);
+  //render each template into the dom
+  console.log();
+  console.log("====");
+  console.log("render templates");
+  Object.keys(app.components).map((component_name, i) => {
+    console.log();
+    console.log(i, ".", "name", component_name);
     if (component_name in app.components) {
       let component_definition = app.components[component_name];
       console.log("component_definition", component_definition);
-
-      //handle for loop
       if ("for_array" in component_definition) {
-        let for_array = component_definition.for_array;
-        console.log("for_array", for_array);
-
-        let for_item = component_definition.for_item || "item";
-        console.log("for_item", for_item);
-
-        let for_index = component_definition.for_index || "index";
-        console.log("for_index", for_index);
-
-        let template_el = app.templates[component_name];
-        console.log("template_el", template_el, template_el.innerHTML);
-
-        const fragment = document.createDocumentFragment();
-
-        for_array.forEach((item, idx) => {
-          //add conditional classes
-          let content = template_el.content.cloneNode(true);
-          if ("class_toggles" in component_definition) {
-            let class_toggles = component_definition.class_toggles;
-            class_toggles.forEach(([class_name, fn]) => {
-              if (fn(item, idx))
-                content.firstElementChild.classList.add(class_name);
-            });
-          }
-          content.firstElementChild.dataset.is_silly = true;
-          fragment.appendChild(content);
-
-          //replace for loop variables
-          const tmp = document.createElement("div");
-          tmp.append(...fragment.childNodes);
-          tmp.innerHTML = tmp.innerHTML
-            .replaceAll("$" + for_item, item)
-            .replaceAll("$" + for_index, idx);
-          while (fragment.firstChild) fragment.removeChild(fragment.firstChild);
-          fragment.append(...tmp.childNodes);
-        });
-
-        template_el.after(fragment);
+        handle_for_loop(component_definition, component_name);
+      } else {
+        handle_default_template(component_definition, component_name);
       }
     }
   });
+}
+
+function collect_templates() {
+  const templates = Array.from(document.querySelectorAll("template"));
+  let nested_templates = [];
+  templates.forEach(
+    (tpl) =>
+      (nested_templates = [
+        ...nested_templates,
+        ...collect_nested_templates(tpl.content),
+      ]),
+  );
+  return { templates, nested_templates };
+}
+
+function collect_nested_templates(fragment) {
+  let nested_templates = [];
+  Array.from(fragment.querySelectorAll("template")).forEach((tpl) => {
+    nested_templates.push(tpl);
+    // dig deeper in case of multi‑level nesting
+    nested_templates = [
+      ...nested_templates,
+      ...collect_nested_templates(tpl.content),
+    ];
+  });
+  return nested_templates;
+}
+
+function handle_for_loop(component_definition, component_name) {
+  console.log();
+  console.log("====");
+  console.log("a. handle for loop");
+  let for_array = component_definition.for_array;
+  console.log("for_array", for_array);
+
+  let for_item = component_definition.for_item || "item";
+  console.log("for_item", for_item);
+
+  let for_index = component_definition.for_index || "index";
+  console.log("for_index", for_index);
+
+  let template_el = app.templates[component_name];
+  console.log("name", component_name);
+  console.log("template_el", template_el, template_el.innerHTML);
+
+  const fragment = document.createDocumentFragment();
+
+  for_array.forEach((item, idx) => {
+    //add conditional classes
+    let content = template_el.content.cloneNode(true);
+    if ("class_toggles" in component_definition) {
+      let class_toggles = component_definition.class_toggles;
+      class_toggles.forEach(([class_name, fn]) => {
+        if (fn(item, idx)) content.firstElementChild.classList.add(class_name);
+      });
+    }
+    content.firstElementChild.dataset.is_silly = true;
+    fragment.appendChild(content);
+
+    //replace for loop variables
+    const tmp = document.createElement("div");
+    tmp.append(...fragment.childNodes);
+    tmp.innerHTML = tmp.innerHTML
+      .replaceAll("$" + for_item, item)
+      .replaceAll("$" + for_index, idx);
+    while (fragment.firstChild) fragment.removeChild(fragment.firstChild);
+    fragment.append(...tmp.childNodes);
+  });
+
+  template_el.after(fragment);
+}
+
+function handle_default_template(component_definition, component_name) {
+  console.log();
+  console.log("====");
+  console.log("b. handle default template");
+  let template_el = app.templates[component_name];
+  console.log("name", component_name);
+  console.log("template_el", template_el);
+  let content = template_el.content.cloneNode(true);
+  content.firstElementChild.dataset.is_silly = true;
+  template_el.after(content);
 }
