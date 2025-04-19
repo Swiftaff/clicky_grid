e647bb29b949489bd56be997eaeb528c908acf40
swiftaff <swiftaff@gmail.com>
2025-04-19 13:40:29 +0800

    remove_child_templates_and_silly_items

--- src/silly.js
+++ src/silly.js
@@ -6,7 +6,7 @@ const app = {
 
 function init(store = {}) {
   console.log("====");
-  console.log("init");
+  console.log("init", store);
   app.store = {
     ...store,
     set: (key, val) => {
@@ -18,15 +18,15 @@ function init(store = {}) {
 }
 
 function reset_dom() {
-  console.log("- reset_dom");
-  remove_previous_silly_elements();
+  //console.log("- reset_dom");
+  remove_previous_silly_elements(document);
   find_silly_templates();
   render_silly_templates_into_dom();
 }
 
 function render_silly_templates_into_dom() {
   //render each template into the dom
-  console.log("- - render templates");
+  //console.log("- - render templates");
   Object.values(app.templates).forEach((t, i) => {
     if ("for" in t.attributes) handle_template__for_loop(t);
     else handle_template__default(t);
@@ -34,7 +34,7 @@ function render_silly_templates_into_dom() {
 }
 
 function collect_root_templates() {
-  console.log("- - - collect root templates");
+  //console.log("- - - collect root templates");
   const templates = Array.from(
     document.querySelectorAll("template[component]"),
   );
@@ -50,14 +50,15 @@ function collect_root_templates() {
 }
 
 function collect_nested_templates(fragment) {
-  console.log("- - - - collect nested templates");
+  //console.log("- - - - collect nested templates");
   let nested_templates = [];
   Array.from(fragment.querySelectorAll("template[component]")).forEach(
     (tpl) => {
       nested_templates.push(tpl);
       nested_templates = [
-        ...nested_templates,
+        //deeper templates need to come first so they are processed first
         ...collect_nested_templates(tpl.content),
+        ...nested_templates,
       ];
     },
   );
@@ -65,19 +66,23 @@ function collect_nested_templates(fragment) {
 }
 
 function handle_template__for_loop(t) {
-  console.log("- - - handle_template__for_loop", t.attributes["for"].value);
+  //console.log("- - - handle_template__for_loop", t.attributes["for"].value);
   let for_array = eval(
     html_replace_placeholders_store(t.attributes["for"].value),
   );
   if (typeof for_array === "number") for_array = [...Array(for_array).keys()];
-  console.log("####", for_array);
+  //console.log("####", for_array);
   let item_key = "item";
-  let item_index = "index";
+  let item_index =
+    "for_index" in t.attributes ? t.attributes.for_index.value : "index";
+  console.log(item_index);
 
   const fragment = document.createDocumentFragment();
   for_array.forEach((item, index) => {
     let content = t.content.cloneNode(true);
+    console.log(content);
     content.firstElementChild.dataset.is_silly = true;
+    console.log(content.firstElementChild);
     replace_placeholders(content, (tmp) => {
       //these act on the contents of the temp div (containing any childNodes)
       html_replace_placeholders_for_loop(
@@ -90,14 +95,17 @@ function handle_template__for_loop(t) {
       html_replace_placeholders_store(tmp);
       //this acts on each of the childNodes
       get_el_with_conditional_classes_for_loop_new(tmp, item, index);
+      //t.content = content;
     });
+    console.log(content.firstElementChild);
     fragment.appendChild(content);
   });
+  remove_child_templates_and_silly_items(fragment);
   t.after(fragment);
 }
 
 function handle_template__default(t) {
-  console.log("- - - handle_template__default");
+  console.log("- - - handle_template__default", t.id);
   const fragment = document.createDocumentFragment();
   let content = t.content.cloneNode(true);
   content.firstElementChild.dataset.is_silly = true;
@@ -107,11 +115,13 @@ function handle_template__default(t) {
     //TODO conditional classes (not for loop - just store?);
   );
   fragment.appendChild(content);
+  if (t.id === "a") console.log("#!#!#!#!#!#!# a", t);
+  remove_child_templates_and_silly_items(fragment);
   t.after(fragment);
 }
 
 function replace_placeholders(fragment, fn) {
-  console.log("- - - - replace placeholders");
+  //console.log("- - - - replace placeholders");
   //run a function on the fragment in a temp div, to search/replace for html placeholders like $index
   const tmp = document.createElement("div");
   tmp.append(...fragment.childNodes);
@@ -138,13 +148,15 @@ function html_replace_placeholders_for_loop(
   item,
   tmp,
 ) {
+  //console.log(tmp.innerHTML);
   tmp.innerHTML = tmp.innerHTML
     .replaceAll("$" + item_key, item)
     .replaceAll("$" + item_index, index);
+  //console.log(tmp.innerHTML);
 }
 
 function html_replace_placeholders_store(tmp) {
-  console.log(Object.keys(app.store), tmp);
+  //console.log(Object.keys(app.store), tmp);
   Object.keys(app.store).forEach((k) => {
     const v = app.store[k];
     //return value for replacing simple values in attributes
@@ -156,19 +168,27 @@ function html_replace_placeholders_store(tmp) {
 }
 
 function find_silly_templates() {
-  console.log("- - find silly templates");
+  //console.log("- - find silly templates");
   const { templates, nested_templates } = collect_root_templates();
   app.templates = [...nested_templates, ...templates];
   console.log("    ", app.templates);
 }
 
-function remove_previous_silly_elements() {
-  console.log("- - remove previous silly elements");
-  const silly_els = document.querySelectorAll("[data-is_silly]");
+function remove_previous_silly_elements(el) {
+  //console.log("- - remove previous silly elements");
+  const silly_els = el.querySelectorAll("[data-is_silly]");
   let silly_templates = Object.values(app.templates).flatMap((tpl) =>
     Array.from(tpl.content.querySelectorAll("[data-is_silly]")),
   );
   silly_templates = [...silly_templates, ...silly_els];
-  console.log("    ", silly_templates);
+  //console.log("    ", silly_templates);
   silly_templates.forEach((t) => t.remove());
+}
+
+function remove_child_templates_and_silly_items(fragment) {
+  let nested = collect_nested_templates(fragment);
+  nested.forEach((n) => {
+    remove_previous_silly_elements(n);
+    n.remove();
+  });
 }
