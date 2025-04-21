e59ae5e2477e078089301ae083f73c50749f9f6d
swiftaff <swiftaff@gmail.com>
2025-04-21 15:50:32 +0800

    autocomplete working

--- src/game.js
+++ src/game.js
@@ -7,6 +7,7 @@ document.addEventListener("alpine:init", () => {
     //updateable but remain across games
     size: 2,
     games: { p1: 0, p2: 0 },
+    autocomplete: true,
     //reset each game
     is_playing: false,
     edges: {},
@@ -44,25 +45,13 @@ document.addEventListener("alpine:init", () => {
         }, 10000);
       }
     },
-    size_update(val, e) {
-      console.log("size_update", val, e);
+    //actions
+    size_update(val) {
       this.size = val;
       this.init_edge_and_box_data();
     },
-    player_toggle() {
-      this.player = this.player === 1 ? 2 : 1;
-    },
-    click_edge(e) {
-      let id = e.target.id;
-      if (this.edges[id]) {
-        //nop if previously clicked
-      } else {
-        this.edges[id] = this.player;
-        this.check_for_closed_box();
-      }
-    },
     start() {
-      console.log("start game");
+      console.log("Clicky grid: starting game");
       this.is_playing = true;
     },
     init_edge_and_box_data() {
@@ -82,32 +71,72 @@ document.addEventListener("alpine:init", () => {
         }
       }
     },
+    click_edge(e) {
+      let id = e.target.id;
+      if (this.edges[id]) {
+        //nop if previously clicked
+      } else {
+        this.edges[id] = this.player;
+        if (this.check_for_closed_box()) {
+          if (this.autocomplete) {
+            let timeout_this = this;
+            setTimeout(function () {
+              let id = timeout_this.get_next_clickable_edge();
+              if (id) timeout_this.click_edge({ target: { id } });
+            }, 50);
+          }
+        } else {
+          this.player_toggle();
+        }
+      }
+    },
     check_for_closed_box() {
+      //console.log("check_for_closed_box");
       let a_box_was_enclosed = false;
-
       for (let col = 0; col <= this.size; col++) {
         for (let row = 0; row <= this.size; row++) {
-          let a = this.get_id("h", row, col);
-          let b = this.get_id("h", row + 1, col);
-          let l = this.get_id("v", row, col);
-          let r = this.get_id("v", row, col + 1);
-          const above = this.edges[a];
-          const below = this.edges[b];
-          const left = this.edges[l];
-          const right = this.edges[r];
+          const { above, below, left, right } = this.get_box_edges(row, col);
           const box_id = this.get_id("b", row, col);
           const is_empty = !(box_id in this.boxes) || this.boxes[box_id] === 0;
           let this_box_was_enclosed =
             above && below && left && right && is_empty;
           if (this_box_was_enclosed) {
+            //console.log("this_box_was_enclosed");
             a_box_was_enclosed = true;
             this.boxes[box_id] = this.player;
-            this.scores[`p${this.player}`] = this.count_player_boxes();
+            this.scores[`p${this.player}`] = this.count_player_boxes;
             this.check_for_winner();
           }
         }
       }
-      if (!a_box_was_enclosed) this.player_toggle();
+      return a_box_was_enclosed;
+    },
+    get_next_clickable_edge() {
+      //console.log("get_next_clickable_edge");
+      // check for 3 sided squares, return the id of one of the clickable edges, if any
+      let remaining_edge_id = "";
+      for (let col = 0; col <= this.size; col++) {
+        for (let row = 0; row <= this.size; row++) {
+          const { a, b, l, r, above, below, left, right } = this.get_box_edges(
+            row,
+            col,
+          );
+          let edge_count = above ? 1 : 0;
+          edge_count += below ? 1 : 0;
+          edge_count += left ? 1 : 0;
+          edge_count += right ? 1 : 0;
+          let this_box_is_closeable = edge_count === 3;
+          if (this_box_is_closeable) {
+            //Concatenate all the id strings, only of empty edges.
+            //Since only one is empty, there should only be a string containing the one remaining id
+            remaining_edge_id = above ? "" : a || "";
+            remaining_edge_id += below ? "" : b || "";
+            remaining_edge_id += left ? "" : l || "";
+            remaining_edge_id += right ? "" : r || "";
+          }
+        }
+      }
+      return remaining_edge_id;
     },
     check_for_winner() {
       this.winner_message = "";
@@ -132,10 +161,27 @@ document.addEventListener("alpine:init", () => {
         if (winner !== this.player) this.player_toggle();
       }
     },
-    count_player_boxes() {
-      return Object.values(this.boxes).reduce((count, value) => {
-        return value === this.player ? count + 1 : count;
-      }, 0);
+    player_toggle() {
+      this.player = this.player === 1 ? 2 : 1;
+    },
+    autocomplete_toggle() {
+      this.autocomplete = !this.autocomplete;
+    },
+    //getters with params
+    get_box_edge_ids(row, col) {
+      const a = this.get_id("h", row, col);
+      const b = this.get_id("h", row + 1, col);
+      const l = this.get_id("v", row, col);
+      const r = this.get_id("v", row, col + 1);
+      return { a, b, l, r };
+    },
+    get_box_edges(row, col) {
+      const { a, b, l, r } = this.get_box_edge_ids(row, col);
+      const above = this.edges[a];
+      const below = this.edges[b];
+      const left = this.edges[l];
+      const right = this.edges[r];
+      return { a, b, l, r, above, below, left, right };
     },
     get_row_col_from_id(id) {
       const parts = id.split("-"); // ["h", "row", "0", "col", "0"]
@@ -151,6 +197,7 @@ document.addEventListener("alpine:init", () => {
     get_id(prefix, row, col) {
       return `${prefix}-row-${row}-col-${col}`;
     },
+    //style getters
     style_square(row, col) {
       return `top:${row * this.cell_size}px; left:${col * this.cell_size}px; width:${this.cell_size}px; height:${this.cell_size}px;`;
     },
@@ -163,6 +210,7 @@ document.addEventListener("alpine:init", () => {
     style_vertex(row, col) {
       return `top: ${row * this.cell_size}px; left: ${col * this.cell_size}px;`;
     },
+    //class getters
     class_edge_color(prefix, row, col) {
       return (
         (this.edges[this.get_id(prefix, row, col)] === 1 && "p1") ||
@@ -175,7 +223,13 @@ document.addEventListener("alpine:init", () => {
         (this.boxes[this.get_id(prefix, row, col)] === 2 && "p2")
       );
     },
-    class_winner_color() {
+    //getters (no params)
+    get count_player_boxes() {
+      return Object.values(this.boxes).reduce((count, value) => {
+        return value === this.player ? count + 1 : count;
+      }, 0);
+    },
+    get class_winner_color() {
       return (
         (this.winner_player === 1 && "p1") ||
         (this.winner_player === 2 && "p2") +
@@ -200,6 +254,9 @@ document.addEventListener("alpine:init", () => {
     },
     get games_p2() {
       return this.games.p2;
+    },
+    get autocomplete_text() {
+      return this.autocomplete ? "autocomplete on" : "autocomplete off";
     },
   }));
 });
